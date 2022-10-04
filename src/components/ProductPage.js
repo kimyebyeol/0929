1da9d8a0b17441fe1dd46df026a4cd629b3a384c
kimyebyeol <byeori0315@naver.com>
2022-10-04 12:00:56 +0900

    상품업로드 컴포턴트 구현

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-const ProductPage = ()=>{
-    const { id } = useParams();
-    return(
-    <><h1>선택하신 상품은 {id} 번 상품입니다</h1></>)}
-export default ProductPage;
+import axios from "axios";
+import "./ProductPage.css"
 
+const ProductPage = () => {
+	const { id } = useParams();
+	const navigate = useNavigate();
+	const [product, setProuct] = useState(null);
+	useEffect(() => {
+		axios
+			.get(`https://e21babb4-4782-4ae4-983c-15c28df715f8.mock.pstmn.io/products/${id}`)
+			.then(function (result) {
+				setProuct(result.data);
+			})
+			.catch(function (error) {
+				console.error(error);
+			});
+	}, []);
+	console.log(product);
+	if (product === null) {
+		return <h1>상품정보를 받고 있습니다...</h1>;
+	}
+	return (
+		<div>
+			<button onClick={() => navigate(-1)} id="back-btn">이전화면</button>
+			<div id="image-box">
+				<img src={`/${product.imageUrl}`} alt={product.name} />
+			</div>
+			<div id="profile-box">
+				<img src="/images/icons/avatar.png" alt={product.seller} />
+				<span className="product-seller">{product.seller}</span>
+			</div>
+			<div id="contents-box">
+				<div id="name">{product.name}</div>
+				<div id="price">{product.price}원</div>
+				<div id="createAt">2022.01.15</div>
+				<div id="description">{product.description}</div>
+			</div>
+		</div>
+	);
+};
+export default ProductPage;
--- src/components/UploadPage.js
+++ src/components/UploadPage.js
@@ -1,2 +1,97 @@
-const UploadPage = ()=>{return(<div>UploadPage</div>)}
+import React from "react";
+import "./UploadPage.css";
+import {
+  Form,
+  Input,
+  InputNumber,
+  Button,
+  Upload,
+  Divider,
+  TextArea,
+} from "antd";
+
+const UploadPage = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  return (
+    <div id="body">
+      <div id="upload-container">
+        <Form name="uploadForm" onFinish={onFinish}>
+          <Form.Item name="upload">
+            <div id="upload-img">
+              <img src="/images/icons/camera.png" alt="" />
+              <span>이미지를 업로드해주세요</span>
+            </div>
+          </Form.Item>
+          <Divider />
+          <Form.Item
+            label={<span className="upload-label">상품명</span>}
+            name="product-name"
+            rules={[
+              { required: true, message: "상품명은 필수 입력사항입니다." },
+            ]}
+          >
+            <Input
+              className="upload-name"
+              placeholder="상품명을  입력해주세요"
+              size="large"
+            />
+          </Form.Item>
+          <Form.Item
+            label={<span className="upload-label">판매자명</span>}
+            name="product-seller"
+            rules={[
+              { required: true, message: "판매자명은 필수 입력사항입니다." },
+            ]}
+          >
+            <Input
+              className="upload-name"
+              placeholder="판매자명을  입력해주세요"
+              size="large"
+            />
+          </Form.Item>
+          <Form.Item
+            label={<span className="upload-label">판매가</span>}
+            name="product-price"
+            rules={[
+              { required: true, message: "판매가는 필수 입력사항입니다." },
+            ]}
+          >
+            <InputNumber
+              className="upload-name"
+              placeholder="판매가를  입력해주세요"
+              size="large"
+              min={0}
+              defaultValue={0}
+            />
+          </Form.Item>
+         
+          <Form.Item
+            label={<span className="upload-label">상품설명</span>}
+            name="product-desc"
+            rules={[
+              { required: true, message: "상품설명는 필수 입력사항입니다." },
+            ]}
+          >
+            <Input.TextArea
+              className="upload-name"
+              placeholder="상품설명를 입력해주세요"
+              size="large"
+              showCount
+              maxLenght={300}
+            />
+          </Form.Item>
+          <Divider />
+          <Form.Item>
+            <Button id="submit-button" size="large" htmlType="submit">
+              상품등록하기
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+};
 export default UploadPage;
